a6181e1900191a3d08a610befd99e5d60cb2f6ec
Angelo <tennflame@gmail.com>
2023-01-12 13:24:30 +0800

    Made Holla Logo clickable (link to '/')

--- frontend/src/components/Header2.jsx
+++ frontend/src/components/Header2.jsx
@@ -6,9 +6,6 @@ import IconButton from '@mui/material/IconButton';
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout, reset } from '../features/auth/authSlice'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 import Holla250 from '../assets/holla250.png'
 export default function Header2() {
@@ -25,15 +22,15 @@ export default function Header2() {
 
 return (
 <nav className="navbar navbar-expand navbar-light m-0 d-flex justify-content-around"
-style={{backgroundColor: '#000000', color: '#FFFF00'}}>
+style={{backgroundColor: '#000000'}}>
     <div className="container-fluid">
-        <img src={Holla250} alt="Holla Logo" style={{height: '50px'}}/>
+    <Link to='/'><img src={Holla250} alt="Holla Logo" style={{height: '50px'}}/></Link>
         <div className="d-flex align-items-center">
             {user ?
                 (
                     <>
-            <span className="text-white me-5"> Hello, {user && user.fname}! </span>
-                        <button type="button" className="btn btn-light ml-auto" onClick={onLogout}
+             <span className="text-white me-5"> Hello, {user && user.fname}! </span>
+                        <button type="button" className="btn btn-danger ml-auto" onClick={onLogout}
                         style={{backgroundColor: '#000000', color: '#FFFF00'}}>
                             <LogoutIcon/> Logout
                         </button>
@@ -41,17 +38,17 @@ style={{backgroundColor: '#000000', color: '#FFFF00'}}>
                 )
                 :
                 (
-                    <>
-                        <Link to='/'> <IconButton>
+                    <div className="d-flex gap-3">
+                        <Link to='/' className="text-decoration-none"> <IconButton className="text-white">
                             <LoginIcon/> Login
                         </IconButton>
                         </Link>
-                        <Link to='/guestregister'> 
-                        <IconButton>
+                        <Link to='/guestregister' className="text-decoration-none"> 
+                        <IconButton className="text-white">
                             <HowToRegIcon/> Register
                         </IconButton>
                         </Link>
-                    </>
+                    </div>
                 )
             }
         </div>
